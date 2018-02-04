582204d70dae054cafc5a5f988b07f9ebd9b9446
Putnik13 <ilja.rumjancev@lenta.ru>
2018-02-04 21:41:28 +0300

    Stable SaturnABC version 0.2

--- src/ru/putnik/saturn/main/Launcher.java
+++ src/ru/putnik/saturn/main/Launcher.java
@@ -1,11 +1,12 @@
-package ru.putnik.saturn.main;/**
- * Created by My Computer on 30.01.2018.
- */
+package ru.putnik.saturn.main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import ru.putnik.saturn.windows.MainWindow;
 
+/**
+ * Created by My Computer on 30.01.2018.
+ */
 public class Launcher{
     public static void main(String[] args) {
         MainWindow.start();
