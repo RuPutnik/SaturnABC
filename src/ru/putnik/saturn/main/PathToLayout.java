580a34cd3d314013163b29d5f2b4e563c293f868
Илья <ilja.rumjancev@lenta.ru>
2018-02-05 20:14:11 +0300

    Update PathToLayout.java

--- src/ru/putnik/saturn/main/PathToLayout.java
+++ src/ru/putnik/saturn/main/PathToLayout.java
@@ -4,6 +4,7 @@ package ru.putnik.saturn.main;
  * Создано 30.12.15
  */
 public interface PathToLayout {
+    //Проверка GitHub
     //Метод-марка для удобного вывода адреса к макету окна
     String getPathToLayout();
 }
